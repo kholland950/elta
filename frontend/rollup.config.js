de4a3dbc398f6060fd9d596459d38b31132704a2
kholland950 <kevinholland94@gmail.com>
2020-11-14 22:59:48 -0500

    Switched to phaser, simple scene and box with gravity

--- frontend/rollup.config.js
+++ frontend/rollup.config.js
@@ -1,22 +1,41 @@
-import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import babel from '@rollup/plugin-babel';
-import { terser } from 'rollup-plugin-terser';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
 
-const production = !process.env.ROLLUP_WATCH;
-
-// rollup.config.js
 export default {
-  input: 'src/main.js',
-  output: {
-    file: 'build/bundle.js',
-    format: 'cjs',
-    sourcemap: true
-  },
-  plugins: [
-    resolve({ browser: true, preferBuiltins: false }),
-    commonjs(),
-    babel({ exclude: 'node_modules/**', babelHelpers: 'bundled' }),
-    production && terser() // minify, but only in production
-  ]
-}
+    //  Our games entry point (edit as required)
+    input: [
+        './src/main.ts'
+    ],
+
+    output: {
+        file: './build/bundle.js',
+        name: 'Game',
+        format: 'iife',
+        sourcemap: true,
+        intro: 'var global = window;'
+    },
+
+    plugins: [
+        //  Parse our .ts source files
+        resolve({
+            extensions: [ '.ts', '.tsx' ]
+        }),
+
+        //  We need to convert the Phaser 3 CJS modules into a format Rollup can use:
+        commonjs({
+            include: [
+                'node_modules/eventemitter3/**',
+                'node_modules/phaser/**'
+            ],
+            exclude: [ 
+                'node_modules/phaser/src/polyfills/requestAnimationFrame.js'
+            ],
+            sourceMap: true,
+            ignoreGlobal: true
+        }),
+
+        //  See https://www.npmjs.com/package/rollup-plugin-typescript2 for config options
+        typescript(),
+    ]
+};
---
+++ frontend/src/main.ts
@@ -0,0 +1,94 @@
+import 'phaser'
+
+export class GameScene extends Phaser.Scene {
+  private square: Phaser.GameObjects.Rectangle & { body: Phaser.Physics.Arcade.Body };
+
+  constructor() {
+    super(sceneConfig);
+  }
+
+  public create() {
+    this.square = this.add.rectangle(400, 400, 100, 100, 0xFFFFFF) as any;
+    this.physics.add.existing(this.square);
+    this.square.body.setDrag(500, 300);
+    this.square.body.setCollideWorldBounds(true);
+  }
+
+  public update() {
+    const cursorKeys = this.input.keyboard.createCursorKeys();
+
+    if (cursorKeys.up.isDown) {
+      this.square.body.setAccelerationY(-5000);
+    } else if (cursorKeys.down.isDown) {
+      this.square.body.setAccelerationY(500);
+    } else {
+      this.square.body.setAccelerationY(0);
+    }
+
+    if (cursorKeys.right.isDown) {
+      this.square.body.setAccelerationX(800);
+    } else if (cursorKeys.left.isDown) {
+      this.square.body.setAccelerationX(-800);
+    } else {
+      this.square.body.setAccelerationX(0);
+    }
+  }
+}
+
+const gameConfig: Phaser.Types.Core.GameConfig = {
+  title: 'Sample',
+
+  type: Phaser.AUTO,
+
+  scale: {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  },
+
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true,
+      gravity: { x: 0, y: 2000 },
+    },
+  },
+
+  parent: 'game',
+  backgroundColor: '#000000',
+
+  scene: GameScene,
+};
+
+const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
+  active: false,
+  visible: false,
+  key: 'Game',
+};
+
+const game = new Phaser.Game(gameConfig);
+
+// let game = new Phaser.Game(config)
+
+// function preload() {
+
+// }
+
+// function create() {
+//   // this.add.image(400, 300, 'sky');
+
+//   var particles = this.add.particles('red');
+
+//   var emitter = particles.createEmitter({
+//     speed: 100,
+//     scale: { start: 1, end: 0 },
+//     blendMode: 'ADD'
+//   });
+
+//   var logo = this.physics.add(400, 100, 'logo');
+
+//   logo.setVelocity(100, 200);
+//   logo.setBounce(1, 1);
+//   logo.setCollideWorldBounds(true);
+
+//   emitter.startFollow(logo);
+// }
